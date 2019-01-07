2a0714e0f7ac0f9c8592b44c6fa797aa6e00a33b
Aashish Karki <aashish.y2z@gmail.com>
2019-01-07 14:25:53 +0545

    Add data and eslint :

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,4 +1,5 @@
 // Modules to control application life and create native browser window
+// eslint-disable-next-line
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
