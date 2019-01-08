be0a4660d2c1605de31dcc93497cdcdd86674d8b
Aashish Karki <aashish.y2z@gmail.com>
2019-01-08 16:52:04 +0545

    Add images

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -13,7 +13,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
   // and load the index.html of the app.
-  const startUrl = process.env.ELECTRON_START_URL || url.format({
+  const startUrl = 'http://localhost:3000/' || url.format({
     pathname: path.join(__dirname, '/../build/index.html'),
     protocol: 'file:',
     slashes: true,
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Background from '../../images/background.png';
 
 const Home = () => (
-  <div />
+  <div>
+    <img src={Background} alt="" />
+  </div>
 );
 
 export default Home;
