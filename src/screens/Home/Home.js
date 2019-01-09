99c1260dd1bcfcfb30ce7fee9195d7cbe07c72c6
Aashish Karki <aashish.y2z@gmail.com>
2019-01-09 11:43:45 +0545

    Add images

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -2,6 +2,19 @@ import React from 'react';
 
 const Home = () => (
   <div className="background">
+    <div className="randomKidOne">
+
+    </div>
+    <div className="randomKidTwo">
+
+    </div>
+    <div className="logo">
+
+  </div>
+    <div className="playButton">
+    </div>
+    <div className="randomKidThree">
+    </div>
   </div>
 );
 
